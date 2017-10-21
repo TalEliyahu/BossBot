f2081f40fb413719ad1d1ff507e5b796052f9a85
zohaib545 <l134031@lhr.nu.edu.pk>
2017-10-22 02:05:32 +0500

    Environment port for express

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ const authController = require('./controllers/auth')
 const group = require ('./routes/group')
 const dashboard = require('./routes/dashboard')
 const auth = require ('./routes/auth')
-const port = process.env.PORT || 3000
+const port = process.env.API_PORT || 3000
 const app = express()
 
 module.exports = {
